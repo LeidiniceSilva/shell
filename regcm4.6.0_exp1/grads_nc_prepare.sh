9de907273a41d7e8f61d8d1e6b06803470655c3d
Leidinice Silva <leidinicesilva@gmail.com>
2018-05-24 12:17:50 -0300

    altered code

--- regcm4.6.0_exp1/grads_nc_prepare.sh
+++ regcm4.6.0_exp1/grads_nc_prepare.sh
@@ -7,8 +7,8 @@
 # To Convert NetCDF in .ctl with GrADSNcPrepare
 
 for VAR in ATM RAD SRF STS; do 
-    for YEAR in `seq 2001 2005`; do         # Change year (Exemple: for YEAR in `seq 2006 2010`; do)
-        for MON in 01 02 03 04 05 06 07 08 09 10 11 12; do
+    for YEAR in `seq 2001 2005`; do       
+        for MON in `seq -w 1 12`; do
     
     	    echo ${VAR} - ${YEAR} - ${MON} 
 	
