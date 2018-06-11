c8395946fc4218b586837cb3f1baf60d6168e779
Leidinice Silva <leidinicesilva@gmail.com>
2018-06-11 09:34:40 -0300

    modified  file

--- regcm4.6.0_exp1/comp_cdo_obs.sh
+++ regcm4.6.0_exp1/comp_cdo_obs.sh
@@ -1,29 +1,46 @@
+#!/bin/bash
+
+#__author__      = 'Leidinice Silva'
+#__email__       = 'leidinicesilva@gmail.br'
+#__date__        = '05/26/18'
+#__description__ = 'Preprocessing the observation data with CDO'
+ 
+
+echo
+echo "--------------- INIT PREPROCESSING OBS ----------------"
 
 
+cd /vol3/nice/obs
+
+
+echo 
+echo "1. Select data (1979010100-2010123100)"
 
 cdo seldate,1979-01-00T00:00:00,2010-12-31T00:00:00 pre_cru_ts4.01_observation_1901-2016.nc pre_cru_ts4.01_observation_1979-2010.nc
-cdo seldate,1979-01-00T00:00:00,2010-12-31T00:00:00 t2m_cru_ts4.01_observation_1901-2016.nc tmp_cru_ts4.01_observation_1979-2010.nc
+cdo seldate,1979-01-00T00:00:00,2010-12-31T00:00:00 t2m_cru_ts4.01_observation_1901-2016.nc t2m_cru_ts4.01_observation_1979-2010.nc
 
-cdo remapbil,pre_cru_ts4.01_observation_1979-2010.nc pr_amz_neb_regcm_exp1_2001-2005.nc pr_amz_neb_regcm_exp1_int_2001-2005.nc
-cdo remapbil,tmp_cru_ts4.01_observation_1979-2010.nc t2m_amz_neb_regcm_exp1_2001-2005.nc t2m_amz_neb_regcm_exp1_int_2001-2005.nc
+echo 
+echo "5. Creating new areas (A1, A2, A3, A4, A5, A6, A7, A8)"
 
-cdo sellonlatbox,30,60,-6,-50 pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_cru_ts4.01_observation_1979-2010_A1.nc
-cdo sellonlatbox,30,60,-6,-50 pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_cru_ts4.01_observation_1979-2010_A2.nc
-cdo sellonlatbox,30,60,-6,-50 pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_cru_ts4.01_observation_1979-2010_A3.nc
-cdo sellonlatbox,30,60,-6,-50 pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_cru_ts4.01_observation_1979-2010_A4.nc
-cdo sellonlatbox,30,60,-6,-50 pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_cru_ts4.01_observation_1979-2010_A5.nc
-cdo sellonlatbox,30,60,-6,-50 pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_cru_ts4.01_observation_1979-2010_A6.nc
-cdo sellonlatbox,30,60,-6,-50 pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_cru_ts4.01_observation_1979-2010_A7.nc
-cdo sellonlatbox,30,60,-6,-50 pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_cru_ts4.01_observation_1979-2010_A8.nc
 
-cdo sellonlatbox,30,60,-6,-50 tmp_cru_ts4.01_observation_1979-2010.nc tmp_amz_neb_cru_ts4.01_observation_1979-2010_A1.nc
-cdo sellonlatbox,30,60,-6,-50 tmp_cru_ts4.01_observation_1979-2010.nc tmp_amz_neb_cru_ts4.01_observation_1979-2010_A2.nc
-cdo sellonlatbox,30,60,-6,-50 tmp_cru_ts4.01_observation_1979-2010.nc tmp_amz_neb_cru_ts4.01_observation_1979-2010_A3.nc
-cdo sellonlatbox,30,60,-6,-50 tmp_cru_ts4.01_observation_1979-2010.nc tmp_amz_neb_cru_ts4.01_observation_1979-2010_A4.nc
-cdo sellonlatbox,30,60,-6,-50 tmp_cru_ts4.01_observation_1979-2010.nc tmp_amz_neb_cru_ts4.01_observation_1979-2010_A5.nc
-cdo sellonlatbox,30,60,-6,-50 tmp_cru_ts4.01_observation_1979-2010.nc tmp_amz_neb_cru_ts4.01_observation_1979-2010_A6.nc
-cdo sellonlatbox,30,60,-6,-50 tmp_cru_ts4.01_observation_1979-2010.nc tmp_amz_neb_cru_ts4.01_observation_1979-2010_A7.nc
-cdo sellonlatbox,30,60,-6,-50 tmp_cru_ts4.01_observation_1979-2010.nc tmp_amz_neb_cru_ts4.01_observation_1979-2010_A8.nc
+for VAR in pre t2m; do
+
+    echo
+    echo "Variable: ${VAR}"
+
+    cdo sellonlatbox,-63,-55,-9,-1 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_amz_neb.nc
+    cdo sellonlatbox,-63,-55,-9,-1 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_A1.nc
+    cdo sellonlatbox,-52.5,-45.5,-3.55,3.5 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_A2.nc
+    cdo sellonlatbox,-75,-71,-15,-11 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_A3.nc
+    cdo sellonlatbox,-78,-72,0,8 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_A4.nc
+    cdo sellonlatbox,-73,-65,-10,-3 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_A5.nc
+    cdo sellonlatbox,-48,-38,-5,-1 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_A6.nc
+    cdo sellonlatbox,-48,-38,-11,-5 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_A7.nc
+    cdo sellonlatbox,-38,-34,-11,-5 ${VAR}_cru_ts4.01_observation_1979-2010.nc ${VAR}_cru_ts4.01_observation_1979-2010_A8.nc
+
+done
+
+
 
 
 
--- regcm4.6.0_exp1/comp_preprocess_cdo_var.sh
+++ regcm4.6.0_exp1/comp_preprocess_cdo_var.sh
@@ -41,54 +41,61 @@ echo "3. Unit convention (mm and celsius)"
 cdo mulc,86400 pre_flux_amz_neb_regcm_exp1_2001-2005.nc pre_amz_neb_regcm_exp1_2001-2005.nc
 cdo addc,-273.15 t2m_K_amz_neb_regcm_exp1_2001-2005.nc t2m_amz_neb_regcm_exp1_2001-2005.nc
 
+echo 
+echo "4. Grid inperpolation (obs ---> model)"
+
+cdo remapbil,pre_cru_ts4.01_observation_1979-2010.nc pre_amz_neb_regcm_exp1_2001-2005.nc pre_amz_neb_regcm_exp1_2001-2005_interp.nc
+cdo remapbil,tmp_cru_ts4.01_observation_1979-2010.nc t2m_amz_neb_regcm_exp1_2001-2005.nc t2m_amz_neb_regcm_exp1_2001-2005_interp.nc
+
 
 echo 
-echo "4. Creating new areas (A1, A2, A3, A4, A5, A6, A7, A8)"
+echo "5. Creating new areas (A1, A2, A3, A4, A5, A6, A7, A8)"
+
 
 for VAR in pre t2m; do
 
     echo
     echo "Variable: ${VAR}"
 	
-    cdo sellonlatbox,-63,-55,-9,-1 ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_A1.nc
-    cdo sellonlatbox,-52.5,-45.5,-3.55,3.5 ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_A2.nc
-    cdo sellonlatbox,-75,-71,-15,-11 ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_A3.nc
-    cdo sellonlatbox,-78,-72,0,8 ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_A4.nc
-    cdo sellonlatbox,-73,-65,-10,-3 ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_A5.nc
-    cdo sellonlatbox,-48,-38,-5,-1 ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_A6.nc
-    cdo sellonlatbox,-48,-38,-11,-5 ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_A7.nc
-    cdo sellonlatbox,-38,-34,-11,-5 ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_A8.nc
+    cdo sellonlatbox,-63,-55,-9,-1 ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A1.nc
+    cdo sellonlatbox,-52.5,-45.5,-3.55,3.5 ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A2.nc
+    cdo sellonlatbox,-75,-71,-15,-11 ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A3.nc
+    cdo sellonlatbox,-78,-72,0,8 ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A4.nc
+    cdo sellonlatbox,-73,-65,-10,-3 ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A5.nc
+    cdo sellonlatbox,-48,-38,-5,-1 ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A6.nc
+    cdo sellonlatbox,-48,-38,-11,-5 ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A7.nc
+    cdo sellonlatbox,-38,-34,-11,-5 ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A8.nc
 
     echo 
-    echo "5. Variable: ${VAR} - Statistics index (mean and sum)"
+    echo "6. Variable: ${VAR} - Statistics index (mean and sum)"
 
-    cdo monsum ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_monsum.nc
-    cdo monmean ${VAR}_amz_neb_regcm_exp1_2001-2005.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_monmean.nc
-    cdo ymonmean ${VAR}_amz_neb_regcm_exp1_2001-2005_monmean.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_clim.nc
-    cdo -b 32 -ymonsub ${VAR}_*_monmean.nc -ymonmean ${VAR}_*_monmean.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_monanom.nc
+    cdo monsum ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_monsum.nc
+    cdo monmean ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_monmean.nc
+    cdo ymonmean ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_monmean.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_clim.nc
+    cdo -b 32 -ymonsub ${VAR}_*_interp_monmean.nc -ymonmean ${VAR}_*_interp_monmean.nc ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_monanom.nc
 
     echo 
-    echo "6. Variable: ${VAR} - Grads NC prepare (./GrADSNcPrepare)"
+    echo "7. Variable: ${VAR} - Grads NC prepare (./GrADSNcPrepare)"
    
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_A1.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_A2.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_A3.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_A4.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_A5.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_A6.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_A7.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_A8.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_monsum.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_monmean.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_clim.nc
-    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_monanom.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A1.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A2.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A3.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A4.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A5.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A6.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A7.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_A8.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_monsum.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_monmean.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_clim.nc
+    ./GrADSNcPrepare ${VAR}_amz_neb_regcm_exp1_2001-2005_interp_monanom.nc
 
 done
 
 
 echo 
-echo "7. Deleted files"
+echo "8. Deleted files"
 
 rm pre_amz_neb_regcm_exp1_*0100.nc
 rm t2m_amz_neb_regcm_exp1_*0100.nc
