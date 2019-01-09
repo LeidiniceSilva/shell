442232d8d6f4da68a9a098d25d2f40db8fbd7a38
Leidinice Silva <leidinicesilva@gmail.com>
2019-01-09 15:10:23 -0300

    update new code

--- regcm4.6.0_exp/download_cmip5_hist.sh
+++ regcm4.6.0_exp/download_cmip5_hist.sh
@@ -6,24 +6,46 @@
 #__description__ = 'Download CMIP5 monthly historical data'
 
 
-MODELS_LIST = ('CanESM2' 'CSIRO')  
-VARS_LIST = ( 'tos_Omon' )  
+# Variables list
+var_list=('evspsbl hfls hfss hurs huss pr ps psl rlds rlus rsds rsus tas tasmax tasmin uas vas')
 
-for MODEL in ${MODELS_LIST[@]}; do 
-    for VAR in ${VARS_LIST[@]}; do
-	    
-    	echo ${MODEL} - ${VAR}
-	
-	PATH="/vol3/disco1/nice/cmip5/cmip5_hist/${MODEL}"
-	
-	echo ${PATH}
-	
-	cd ${PATH}
-	    
-	mkdir ${MODEL}
+for var in ${var_list[@]}; do
+    for year in $(/usr/bin/seq -w 1850 10 1990) ; do
 
+	path="/home/nice/Downloads"
+	cd ${path}
 
-	/usr/bin/wget -N http://clima-dods.ictp.it/Data/CMIP5/monthly/hist/${MODEL}/*.nc
+	in_date=${year}
 
+	fi_date=$(date -I -d "${year}-12-28 + 9 years" | sed -n '1p' | cut -d '-' -f 1 )
+
+	# Models list
+	model=('CMCC-CM')
+
+	# Experiment
+	exp_name=('historical_r1i1p1')
+
+	echo ${var}"_"${model}"_"${exp_name}"_"${in_date}"-"${fi_date}
+	echo
+
+	# Creating path model
+	model_path=${path}/${model}
+
+	if [ ! -d ${model} ]
+	then
+	mkdir ${model}
+	else
+	echo "Directory already exists"
+	echo
+	fi
+
+	cd ${model}
+	pwd
+
+	/usr/bin/wget -N -c http://clima-dods.ictp.it/Data/CMIP5/monthly/hist/${model}/${var}_Amon_${model}_${exp_name}_${in_date}01-${fi_date}12.nc
+
+	echo "Saving files in path:" ${var}"_"${model}"_"${exp_name}"_"${in_date}"-"${fi_date}
+	echo
+    
     done
-done	
+done
