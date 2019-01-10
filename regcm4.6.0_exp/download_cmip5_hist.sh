ff5ae178698d66da323a06eb8194bb6bb9d57970
Leidinice Silva <leidinicesilva@gmail.com>
2019-01-10 15:30:36 -0300

    modified new code

--- regcm4.6.0_exp/download_cmip5_hist.sh
+++ regcm4.6.0_exp/download_cmip5_hist.sh
@@ -7,7 +7,7 @@
 
 
 # Variables list
-var_list=('evspsbl hfls hfss hurs huss pr ps psl rlds rlus rsds rsus tas tasmax tasmin uas vas')
+var_list=('hfls hfss pr ps psl rlds rlus rsds rsus tas tasmax tasmin uas vas')
 
 for var in ${var_list[@]}; do
     for year in $(/usr/bin/seq -w 1850 10 1990) ; do
@@ -15,17 +15,17 @@ for var in ${var_list[@]}; do
 	path="/home/nice/Downloads"
 	cd ${path}
 
-	in_date=${year}
+	# Model name
+	model=('CMCC-CMS')
 
+	# Experiment name
+	exp=('historical_r1i1p1')
+
+	# Date
+	in_date=${year}
 	fi_date=$(date -I -d "${year}-12-28 + 9 years" | sed -n '1p' | cut -d '-' -f 1 )
 
-	# Models list
-	model=('CMCC-CM')
-
-	# Experiment
-	exp_name=('historical_r1i1p1')
-
-	echo ${var}"_"${model}"_"${exp_name}"_"${in_date}"-"${fi_date}
+	echo "Starting download:" ${var}"_"${model}"_"${exp}"_"${in_date}"-"${fi_date}
 	echo
 
 	# Creating path model
@@ -35,16 +35,17 @@ for var in ${var_list[@]}; do
 	then
 	mkdir ${model}
 	else
+
 	echo "Directory already exists"
 	echo
+
 	fi
 
 	cd ${model}
-	pwd
 
-	/usr/bin/wget -N -c http://clima-dods.ictp.it/Data/CMIP5/monthly/hist/${model}/${var}_Amon_${model}_${exp_name}_${in_date}01-${fi_date}12.nc
+	/usr/bin/wget -N -c http://clima-dods.ictp.it/Data/CMIP5/monthly/hist/${model}/${var}_Amon_${model}_${exp}_${in_date}01-${fi_date}12.nc
 
-	echo "Saving files in path:" ${var}"_"${model}"_"${exp_name}"_"${in_date}"-"${fi_date}
+	echo "Ending download:" ${var}_Amon_${model}_${exp}_${in_date}01-${fi_date}12.nc
 	echo
     
     done
