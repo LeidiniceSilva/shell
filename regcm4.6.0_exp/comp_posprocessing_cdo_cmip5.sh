4c8907c4f94d55b00df19ceffbc0fdc29bfc25e6
Leidinice Silva <leidinicesilva@gmail.com>
2019-01-29 11:10:11 -0300

    modified code

--- regcm4.6.0_exp/comp_posprocessing_cdo_cmip5.sh
+++ regcm4.6.0_exp/comp_posprocessing_cdo_cmip5.sh
@@ -2,61 +2,66 @@
 
 #__author__      = 'Leidinice Silva'
 #__email__       = 'leidinicesilva@gmail.br'
-#__date__        = '01/11/19'
-#__description__ = 'Posprocessing the CMIP5 model data with CDO'
+#__date__        = '01/26/19'
+#__description__ = 'Posprocessing the RegCM4.6.0 model data with CDO'
  
 
 echo
 echo "--------------- INIT POSPROCESSING CMIP5 MODELS ----------------"
 
-var_list=('pr')
-model_list=('BCC-CSM1-1')
+# Variables list
+var_list=('pr')     
+
+# Models list
+model_list=('BCC-CSM1.1M' 'BNU-ESM')
+
+# model_list=( 'BCC-CSM1.1' 'BCC-CSM1.1M' 'BNU-ESM' 'CanESM2' 'CMCC-CM' 'CMCC-CMS' 'CNRM-CM5' 'CSIRO-ACCESS-1' 'CSIRO-ACCESS-3' 'CSIRO-MK36' 'EC-EARTH' 'FIO-ESM' 'GFDL-ESM2G' 'GFDL-ESM2M' 'GISS-E2-H-CC' 'GISS-E2-H' 'GISS-E2-R-CC' 'GISS-E2-R' 'HadGEM2-AO' 'HadGEM2-CC' 'HadGEM2-ES' 'INMCM4' 'IPSL-CM5A-LR' 'IPSL-CM5A-MR' 'IPSL-CM5B-LR' 'LASG-FGOALS-G2' 'LASG-FGOALS-S2' 'MIROC5' 'MIROC-ESM-CHEM' 'MIROC-ESM' 'MPI-ESM-LR' 'MPI-ESM-MR' 'MRI-CGCM3' 'NCAR-CCSM4' 'NCAR-CESM1-BGC' 'NCAR-CESM1-CAM5' 'NorESM1-ME' 'NorESM1-M')    
 
 for var in ${var_list[@]}; do
     for model in ${model_list[@]}; do
 
-	path="/home/nice/Downloads"
+	path="/home/nice/Documentos/cmip_data/cmip5_hist"
 	cd ${path}
-
-	# Experiment name
-	exp=('historical_r1i1p1')
-
-	# Date
-	date=('185001-201212')
 	
 	echo
-	echo "Starting with:" ${path}"/"${var}"_""Amon""_"${model}"_"${exp}"_"${date}".nc"
+	echo ${path}
+
+	# Experiment name
+	exp=( 'historical_r1i1p1' ) 
+
+	# Init date and final date
+	ini_date=$( ls ${path_models}${var}_Amon_${model}_${exp}*.nc | sed -n '1p' | cut -d '_' -f 6 | cut -d '-' -f 1 )
+	end_date=$( ls ${path_models}${var}_Amon_${model}_${exp}*.nc | sed -n "${fl_nbr}p" | cut -d '_' -f 6 | cut -d '-' -f 2 )
+	
+	echo
+	echo "Posprocessing file:" ${var}"_Amon_"${model}"_"${exp}"_"${ini_date}"-"${end_date} 
+
+	echo
+	echo "1. Select date: 197512-200511"
+	cdo seldate,1975-12-00T00:00:00,2005-12-31T00:00:00 ${var}_Amon_${model}_${exp}_${ini_date}-${end_date} ${var}_Amon_${model}_${exp}_197512-200511.nc
+	
+	echo
+	echo "2. Convert unit: mm and ºC"
+	cdo mulc,86400 ${var}_Amon_${model}_${exp}_197512-200511.nc ${var}_Amon_${model}_${exp}_197512-200511_mm.nc
+	
+	echo
+	echo "3. Interpolate area: r720x360"
+	cdo remapbil,r720x360 ${var}_Amon_${model}_${exp}_197512-200511_mm.nc ${var}_Amon_${model}_${exp}_197512-200511_mm_newgrid.nc
+	
+	echo
+	echo "4. Select new area: amz_neb (-85,-15,-20,10)"
+	cdo sellonlatbox,-85,-15,-20,10 ${var}_Amon_${model}_${exp}_197512-200511_mm_newgrid.nc pre_amz_neb_Amon_${model}_${exp}_197512-200511.nc
 
 	echo 
-	echo "1. Select data: 198001-200512"
+	echo "5. Deleting file"
 
-	cdo seldate,1980-01-00T00:00:00,2005-12-31T00:00:00 ${var}_Amon_${model}_${exp}_${date}.nc ${var}_Amon_${model}_${exp}_198001-200512.nc 
-
-	echo 
-	echo "2. Unit convention: flux to mm"
-
-	cdo mulc,86400 ${var}_Amon_${model}_${exp}_198001-200512.nc ${var}_Amon_${model}_${exp}_mon_198001-200512.nc 
-
-	echo 
-	echo "3. Remapbil: r360x180"
-
-	cdo remapbil,r360x180, ${var}_Amon_${model}_${exp}_mon_198001-200512.nc ${var}_newgrid_Amon_${model}_${exp}_mon_198001-200512.nc
-
-	echo 
-	echo "4. Sellonlatbox: -85,-15,-20,10"
-
-	cdo sellonlatbox,-85,-15,-20,10 ${var}_newgrid_Amon_${model}_${exp}_mon_198001-200512.nc ${var}_amz_neb_Amon_${model}_${exp}_mon_198001-200512.nc
-
-	echo 
-	echo "5. Deleted files"
-
-	rm ${var}_Amon_${model}_${exp}_198001-200512.nc
-        rm ${var}_Amon_${model}_${exp}_mon_198001-200512.nc
-	rm ${var}_newgrid_Amon_${model}_${exp}_mon_198001-200512.nc
+	rm ${var}_Amon_${model}_${exp}_197512-200511.nc
+	rm ${var}_Amon_${model}_${exp}_197512-200511_mm.nc
+	rm ${var}_Amon_${model}_${exp}_197512-200511_mm_newgrid.nc
 
     done
 done
 
 echo
-echo "--------------- THE END POSPROCESSING CMIP5 MODELS ----------------"
+echo "--------------- THE END POSPROCESSING MODEL ----------------"
 
