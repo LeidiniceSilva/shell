0a42bb2b919ca7dfc5e46804dc0939fb5ce3270b
Leidinice Silva <leidinicesilva@gmail.com>
2019-04-05 17:35:13 -0300

    add new code

--- regcm4.6.0_exp/download_cmip5_hadgem2-es_6hLev.sh
+++ regcm4.6.0_exp/download_cmip5_hadgem2-es_6hLev.sh
@@ -10,14 +10,26 @@ echo
 echo "--------------- INIT DOWNLOAD HadGEM2-ES CMIP5 MODEL ----------------"
 
 # Variables list
-var_list=('hus ps ta ua va')
+var_list=('hus ta ua va ps')
 
 for var in ${var_list[@]}; do
-    for year in $(/usr/bin/seq -w 1949 2005); do
-	for mon in $(/usr/bin/seq -w 03 3 12); do
+
+    path="/vol3/disco1/nice/data_file/cmip_data/cmip5/hadgem2-es_downscaling/RF"
+    cd ${path}
 	    
-            path="/vol3/disco1/nice/data_file/cmip_data/cmip5/hadgem2-es_downscaling"
-	    cd ${path}
+    # Creating path model
+    var_path=${path}/${var}
+    if [ ! -d ${var} ]
+    then
+    mkdir ${var}
+    else
+    echo "Directory already exists"
+    echo
+    fi
+    cd ${var}
+
+    for year in $(/usr/bin/seq -w 1949 2005); do
+	for mon in $(/usr/bin/seq -w 3 3 12); do
 
 	    # Model time
 	    time=('6hrLev')
@@ -33,30 +45,17 @@ for var in ${var_list[@]}; do
 
 	    if [ ${mon} == '12' ]
 	    then
-	    fi_date=$(date -I -d "${year}${mon}-12-28 - 9 months" | sed -n '1p' | cut -d '-' -f 1 )
+	    fi_date=$(date -I -d "${year}${mon} - 9 months" | sed -n '1p' | cut -d '-' -f 1 )
 	    else
-	    fi_date=$(date -I -d "${year}${mon}-12-28 + 3 months" | sed -n '1p' | cut -d '-' -f 1 )
+	    fi_date=$(date -I -d "${year}${mon} + 3 months" | sed -n '1p' | cut -d '-' -f 1 )
 	    fi
 
-	    echo "Starting download: ${var}_${time}_${model}_${exp}_${in_date}_${fi_date}.nc"
+	    echo "Starting download: ${var}_${time}_${model}_${exp}_${in_date}0106_${fi_date}0100.nc"
 	    echo 
 	
-	    # Creating path model
-	    var_path=${path}/${var}
-
-	    if [ ! -d ${var} ]
-	    then
-	    mkdir ${var}
-	    else
-	    echo "Directory already exists"
-	    echo
-	    fi
-
-	    cd ${var}
-
 	    /usr/bin/wget -N -c http://clima-dods.ictp.it/Data/RegCM_Data/HadGEM2/RF/${var}/${var}_${time}_${model}_${exp}_${in_date}0106_${fi_date}0100.nc
 
-	    echo "Ending download: ${var}_${time}_${model}_${exp}_${in_date}_${fi_date}.nc"
+	    echo "Ending download: ${var}_${time}_${model}_${exp}_${in_date}0106_${fi_date}0100.nc"
 	    echo
 
 	done    
