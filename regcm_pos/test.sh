df5ec6f4d1b3a2e74439b8036951842924568504
Leidinice Silva <leidinicesilva@gmail.com>
2022-06-03 15:49:38 -0300

    update scripts

--- regcm_pos/test.sh
+++ regcm_pos/test.sh
@@ -1,4 +1,18 @@
 
+var="pr"
+exp="historical_r1i1p1"
+
+model_list=( 'BCC-CSM1.1' 'BCC-CSM1.1M' 'BNU-ESM' 'CanESM2' 'CMCC-CM' 'CMCC-CMS' 'CNRM-CM5' 'CSIRO-ACCESS-1' 'CSIRO-ACCESS-3' 'CSIRO-MK36' 'ensmean_cmip5' 'FIO-ESM' 'GFDL-ESM2G' 'GFDL-ESM2M' 'GISS-E2-H-CC' 'GISS-E2-H' 'GISS-E2-R-CC' 'GISS-E2-R' 'HadGEM2-AO' 'HadGEM2-CC' 'HadGEM2-ES' 'INMCM4' 'IPSL-CM5A-LR' 'IPSL-CM5A-MR' 'IPSL-CM5B-LR' 'LASG-FGOALS-G2' 'LASG-FGOALS-S2' 'MIROC5' 'MIROC-ESM-CHEM' 'MIROC-ESM' 'MPI-ESM-LR' 'MPI-ESM-MR' 'MRI-CGCM3' 'NCAR-CCSM4' 'NCAR-CESM1-BGC' 'NCAR-CESM1-CAM5' 'NorESM1-ME' 'NorESM1-M')    
+
+for model in ${model_list[@]}; do
+
+	echo
+	echo "Model: " ${model}
+	
+	cdo yearmean ${var}_amz_neb_Amon_${model}_${exp}_197512-200511.nc ${var}_amz_neb_Ayr_${model}_${exp}_197512-200511.nc
+
+done
+
 var="tas"
 sim="Amon_HadGEM2-ES_hist_r1i1p1"
 tim="ann"
@@ -10,15 +24,15 @@ echo ${tim}
 obs_dir="/home/nice/Documents/dataset/obs/reg_exp1"
 sim_dir="/home/nice/Documents/dataset/gcm/reg_exp1/hist"
 
-#~ cdo -r -timselavg,3 -selmon,9,10,11 pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_son_1986-2005_lonlat_seamask.nc
-#~ cdo -r -timselavg,3 -selmon,6,7,8 pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_jja_1986-2005_lonlat_seamask.nc
-#~ cdo -r -timselavg,3 -selmon,3,4,5 pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mam_1986-2005_lonlat_seamask.nc
-#~ cdo -r -timselavg,3 -selmon,1,2,12 pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_djf_1986-2005_lonlat_seamask.nc
-#~ cdo yearmean pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_ann_1986-2005_lonlat_seamask.nc
+cdo -r -timselavg,3 -selmon,9,10,11 pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_son_1986-2005_lonlat_seamask.nc
+cdo -r -timselavg,3 -selmon,6,7,8 pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_jja_1986-2005_lonlat_seamask.nc
+cdo -r -timselavg,3 -selmon,3,4,5 pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mam_1986-2005_lonlat_seamask.nc
+cdo -r -timselavg,3 -selmon,1,2,12 pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_djf_1986-2005_lonlat_seamask.nc
+cdo yearmean pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_mon_1986-2005_lonlat_seamask.nc pr_amz_neb_Amon_HadGEM2-ES_hist_r1i1p1_ann_1986-2005_lonlat_seamask.nc
 
-#~ cdo sellonlatbox,-68,-52,-12,-3 ${obs_dir}/${var}_amz_neb_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc ${obs_dir}/${var}_samz_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc
-#~ cdo sellonlatbox,-40,-35,-16,-3 ${obs_dir}/${var}_amz_neb_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc ${obs_dir}/${var}_eneb_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc
-#~ cdo sellonlatbox,-50.5,-42.5,-15,-2.5 ${obs_dir}/${var}_amz_neb_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc ${obs_dir}/${var}_matopiba_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc
+cdo sellonlatbox,-68,-52,-12,-3 ${obs_dir}/${var}_amz_neb_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc ${obs_dir}/${var}_samz_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc
+cdo sellonlatbox,-40,-35,-16,-3 ${obs_dir}/${var}_amz_neb_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc ${obs_dir}/${var}_eneb_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc
+cdo sellonlatbox,-50.5,-42.5,-15,-2.5 ${obs_dir}/${var}_amz_neb_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc ${obs_dir}/${var}_matopiba_cru_ts4.04_obs_${tim}_1986-2005_lonlat.nc
 
 cdo sellonlatbox,-68,-52,-12,-3 ${sim_dir}/${var}_amz_neb_${sim}_${tim}_1986-2005_lonlat_seamask.nc ${sim_dir}/${var}_samz_${sim}_${tim}_1986-2005_lonlat_seamask.nc
 cdo sellonlatbox,-40,-35,-16,-3 ${sim_dir}/${var}_amz_neb_${sim}_${tim}_1986-2005_lonlat_seamask.nc ${sim_dir}/${var}_eneb_${sim}_${tim}_1986-2005_lonlat_seamask.nc
