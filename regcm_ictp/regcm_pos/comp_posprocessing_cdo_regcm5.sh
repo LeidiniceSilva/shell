7919688d70627657773887bfaed57fe1ca382d9f
Leidinice Silva <leidinicesilva@gmail.com>
2023-11-20 18:42:35 +0100

    update scripts

--- regcm_ictp/regcm_pos/comp_posprocessing_cdo_regcm5.sh
+++ regcm_ictp/regcm_pos/comp_posprocessing_cdo_regcm5.sh
@@ -9,8 +9,8 @@
 echo
 echo "--------------- INIT POSPROCESSING MODEL ----------------"
 
-NAME="SAM-22"
-DIR="/home/mda_silv/scratch/test1/output"
+NAME="SAM-3km"
+DIR="/marconi/home/userexternal/mdasilva/user/mdasilva/sam_3km/output"
 
 echo
 cd ${DIR}
@@ -21,60 +21,48 @@ echo "1. Select variable"
 
 for MON in `seq -w 01 12`; do
 
-    #echo "Data: ${MON}/2000"
-    #cdo selname,pr ${NAME}_STS.2000${MON}0100.nc pr_${NAME}_2000${MON}0100.nc
-    #cdo selname,tas ${NAME}_STS.2000${MON}0100.nc tas_${NAME}_2000${MON}0100.nc
-    cdo selname,cl ${NAME}_.2000${MON}0100.nc cl_${NAME}_2000${MON}0100.nc
-    cdo selname,o3 ${NAME}_RAD.2000${MON}0100.nc o3_${NAME}_2000${MON}0100.nc
-    cdo selname,rtnscl ${NAME}_RAD.2000${MON}0100.nc rtnscl_${NAME}_2000${MON}0100.nc
-    cdo selname,rsnscl ${NAME}_RAD.2000${MON}0100.nc rsnscl_${NAME}_2000${MON}0100.nc
-    cdo selname,tauci ${NAME}_RAD.2000${MON}0100.nc tauci_${NAME}_2000${MON}0100.nc
+    cdo selname,pr ${NAME}_STS.2018${MON}0100.nc pr_${NAME}_2018${MON}0100.nc
+    cdo selname,tas ${NAME}_STS.2018${MON}0100.nc tas_${NAME}_2018${MON}0100.nc
+    cdo selname,tasmax ${NAME}_STS.2018${MON}0100.nc tasmax_${NAME}_2018${MON}0100.nc
+    cdo selname,tasmin ${NAME}_STS.2018${MON}0100.nc tasmin_${NAME}_2018${MON}0100.nc
+    cdo selname,cl ${NAME}_RAD.2018${MON}0100.nc cl_${NAME}_2018${MON}0100.nc
+    cdo selname,clt ${NAME}_SRF.2018${MON}0100.nc clt_${NAME}_2018${MON}0100.nc
+    cdo selname,cli ${NAME}_ATM.2018${MON}0100.nc cli_${NAME}_2018${MON}0100.nc
+    cdo selname,clw ${NAME}_ATM.2018${MON}0100.nc clw_${NAME}_2018${MON}0100.nc
 
 done	
 
 echo 
 echo "2. Concatenate data"
 
-#cdo cat pr_${NAME}_2000*0100.nc pr_${NAME}_2000.nc 
-#cdo cat tas_${NAME}_2000*0100.nc tas_${NAME}_2000.nc 
-cdo cat cl_${NAME}_2000*0100.nc cl_${NAME}_2000.nc 
-cdo cat o3_${NAME}_2000*0100.nc o3_${NAME}_2000.nc 
-cdo cat rtnscl_${NAME}_2000*0100.nc rtnscl_${NAME}_2000.nc 
-cdo cat rsnscl_${NAME}_2000*0100.nc rsnscl_${NAME}_2000.nc 
-cdo cat tauci_${NAME}_2000*0100.nc tauci_${NAME}_2000.nc 
-
-echo
-echo "3. Convert calendar: standard"
-
-#cdo setcalendar,standard pr_${NAME}_2000.nc pr_${NAME}_day_2000.nc
-#cdo setcalendar,standard tas_${NAME}_2000.nc tas_${NAME}_day_2000.nc
-cdo setcalendar,standard cl_${NAME}_2000.nc tas_${NAME}_day_2000.nc
-cdo setcalendar,standard o3_${NAME}_2000.nc tas_${NAME}_day_2000.nc
-cdo setcalendar,standard rtnscl_${NAME}_2000.nc rtnscl_${NAME}_day_2000.nc
-cdo setcalendar,standard rsnscl_${NAME}_2000.nc rsnscl_${NAME}_day_2000.nc
-cdo setcalendar,standard tauci_${NAME}_2000.nc tauci_${NAME}_day_2000.nc
+cdo cat pr_${NAME}_2018*0100.nc pr_${NAME}_day_2018.nc 
+cdo cat tas_${NAME}_2018*0100.nc tas_${NAME}_day_2018.nc 
+cdo cat tasmax_${NAME}_2018*0100.nc tasmax_${NAME}_day_2018.nc 
+cdo cat tasmin_${NAME}_2018*0100.nc tasmin_${NAME}_day_2018.nc 
+cdo cat cl_${NAME}_2018*0100.nc cl_${NAME}_2018.nc
+cdo cat clt_${NAME}_2018*0100.nc clt_${NAME}_2018.nc  
+cdo cat cli_${NAME}_2018*0100.nc cli_${NAME}_2018.nc 
+cdo cat clw_${NAME}_2018*0100.nc clw_${NAME}_2018.nc 
 
 echo 
-echo "4. Remapbil (Precipitation and Temperature 2m: South America)"
+echo "2. Calculate monthly avg"
 
-#/home/mda_silv/github_projects/shell/regcm_pos/./regrid pr_${NAME}_day_2000.nc -57.89861,18.49594,0.22 -105.8313,-16.58986,0.22 bil
-#/home/mda_silv/github_projects/shell/regcm_pos/./regrid tas_${NAME}_day_2000.nc -57.89861,18.49594,0.22 -105.8313,-16.58986,0.22 bil 
-/home/mda_silv/github_projects/shell/regcm_pos/./regrid cl_${NAME}_day_2000.nc -57.89861,18.49594,0.22 -105.8313,-16.58986,0.22 bil 
-/home/mda_silv/github_projects/shell/regcm_pos/./regrid o3_${NAME}_day_2000.nc -57.89861,18.49594,0.22 -105.8313,-16.58986,0.22 bil 
-/home/mda_silv/github_projects/shell/regcm_pos/./regrid rtnscl_${NAME}_day_2000.nc -57.89861,18.49594,0.22 -105.8313,-16.58986,0.22 bil 
-/home/mda_silv/github_projects/shell/regcm_pos/./regrid rsnscl_${NAME}_day_2000.nc -57.89861,18.49594,0.22 -105.8313,-16.58986,0.22 bil 
-/home/mda_silv/github_projects/shell/regcm_pos/./regrid tauci_${NAME}_day_2000.nc -57.89861,18.49594,0.22 -105.8313,-16.58986,0.22 bil 
+cdo monmean cl_${NAME}_2018.nc cl_${NAME}_mon_2018.nc
+cdo monmean clt_${NAME}_2018.nc clt_${NAME}_mon_2018.nc  
+cdo monmean cli_${NAME}_2018.nc cli_${NAME}_mon_2018.nc
+cdo monmean clw_${NAME}_2018.nc clw_${NAME}_mon_2018.nc
 
 echo 
-echo "5. Unit convention (mm/d and celsius)"
+echo "3. Regrid output"
 
-#cdo mulc,86400 pr_${NAME}_day_2000_lonlat.nc pr_${NAME}_Reg5_day_2000_lonlat.nc
-#cdo addc,-273.15 tas_${NAME}_day_2000_lonlat.nc tas_${NAME}_Reg5_day_2000_lonlat.nc
-cp cl_${NAME}_day_2000_lonlat.nc cl_${NAME}_Reg5_day_2000_lonlat.nc 
-cp o3_${NAME}_day_2000_lonlat.nc o3_${NAME}_Reg5_day_2000_lonlat.nc 
-cp rtnscl_${NAME}_day_2000_lonlat.nc rtnscl_${NAME}_Reg5_day_2000_lonlat.nc 
-cp rsnscl_${NAME}_day_2000_lonlat.nc rsnscl_${NAME}_Reg5_day_2000_lonlat.nc 
-cp tauci_${NAME}_day_2000_lonlat.nc tauci_${NAME}_Reg5_day_2000_lonlat.nc 
+/marconi/home/userexternal/mdasilva/github_projects/shell/regcm_ufrn/regcm_pos/./regrid pr_${NAME}_day_2018.nc -35.70235,-11.25009,0.03 -78.66277,-35.48362,0.03 bil
+/marconi/home/userexternal/mdasilva/github_projects/shell/regcm_ufrn/regcm_pos/./regrid tas_${NAME}_day_2018.nc -35.70235,-11.25009,0.03 -78.66277,-35.48362,0.03 bil
+/marconi/home/userexternal/mdasilva/github_projects/shell/regcm_ufrn/regcm_pos/./regrid tasmax_${NAME}_day_2018.nc -35.70235,-11.25009,0.03 -78.66277,-35.48362,0.03 bil
+/marconi/home/userexternal/mdasilva/github_projects/shell/regcm_ufrn/regcm_pos/./regrid tasmin_${NAME}_day_2018.nc -35.70235,-11.25009,0.03 -78.66277,-35.48362,0.03 bil
+/marconi/home/userexternal/mdasilva/github_projects/shell/regcm_ufrn/regcm_pos/./regrid cl_${NAME}_mon_2018.nc -35.70235,-11.25009,0.03 -78.66277,-35.48362,0.03 bil
+/marconi/home/userexternal/mdasilva/github_projects/shell/regcm_ufrn/regcm_pos/./regrid clt_${NAME}_mon_2018.nc -35.70235,-11.25009,0.03 -78.66277,-35.48362,0.03 bil
+/marconi/home/userexternal/mdasilva/github_projects/shell/regcm_ufrn/regcm_pos/./regrid cli_${NAME}_mon_2018.nc -35.70235,-11.25009,0.03 -78.66277,-35.48362,0.03 bil
+/marconi/home/userexternal/mdasilva/github_projects/shell/regcm_ufrn/regcm_pos/./regrid clw_${NAME}_mon_2018.nc -35.70235,-11.25009,0.03 -78.66277,-35.48362,0.03 bil
 
 echo
 echo "--------------- THE END POSPROCESSING MODEL ----------------"
