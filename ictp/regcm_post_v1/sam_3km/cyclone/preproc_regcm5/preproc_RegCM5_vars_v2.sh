57334b6f91d0b7a756bd8471be33a106e652746a
Leidinice Silva <leidinicesilva@gmail.com>
2024-04-09 19:08:59 +0200

    update scripts

---
+++ ictp/regcm_post_v1/sam_3km/cyclone/preproc_regcm5/preproc_RegCM5_vars_v2.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#__author__      = 'Leidinice Silva'
+#__email__       = 'leidinicesilva@gmail.com'
+#__date__        = 'Nov 20, 2023'
+#__description__ = 'Posprocessing the RegCM5 output with CDO'
+ 
+{
+
+set -eo pipefail
+CDO(){
+  cdo -O -L -f nc4 -z zip $@
+}
+
+VAR_LIST="ta"
+#VAR_LIST="psl ta uas vas"
+EXP="SAM-3km"
+MODEL="ECMWF-ERA5_evaluation_r1i1p1f1_ICTP-RegCM5"
+DT="2018-2021"
+
+DIR_IN="/marconi/home/userexternal/mdasilva/user/mdasilva/SAM-3km/NoTo-SAM"
+BIN="/marconi/home/userexternal/mdasilva/github_projects/shell/ictp/regcm_post_v2/scripts/bin"
+
+echo
+echo "--------------- INIT POSPROCESSING MODEL ----------------"
+
+for VAR in ${VAR_LIST[@]}; do
+
+    DIR_OUT="/marconi/home/userexternal/mdasilva/user/mdasilva/SAM-3km/post_cyclone/rcm/regcm5/${VAR}"
+    
+    echo
+    cd ${DIR_OUT}
+    echo ${DIR_OUT}
+    
+    for YEAR in `seq -w 2018 2021`; do
+        for MON in `seq -w 01 12`; do
+	    
+	    echo
+	    echo "1. Select variable: ${VAR}"
+	    CDO selname,${VAR} ${DIR_IN}/pressure/${EXP}_ATM.${YEAR}${MON}0100_pressure.nc ${VAR}_${EXP}_${YEAR}${MON}0100.nc
+	    
+	    echo
+	    echo "2. Select levels"
+	    CDO sellevel,925 ${VAR}_${EXP}_${YEAR}${MON}0100.nc ${VAR}_${EXP}_${MODEL}_6hr_${YEAR}${MON}0100.nc
+
+
+	    echo
+	    echo "Delete files"
+	    rm ${VAR}_${EXP}_${YEAR}${MON}0100.nc
+	    
+        done
+    done	
+
+done
+    
+echo
+echo "--------------- THE END POSPROCESSING MODEL ----------------"
+
+}
--- ictp/regcm_post_v2/scripts/run_all_postproc.sh
+++ ictp/regcm_post_v2/scripts/run_all_postproc.sh
@@ -30,11 +30,11 @@ n=$this_domain
 [[ $n = Europe ]] && domdir=EUR11
 [[ $n = WMediterranean ]] && domdir=WMD03
 
-export rdir=/marconi/home/userexternal/mdasilva/user/mdasilva/SAM-3km_v2
-yrs=2018-2018
+export rdir=/marconi/home/userexternal/mdasilva/user/mdasilva/SAM-3km
+yrs=2018-2021
 email="mda_silv@ictp.it"
 
-run_postproc="1 0 0 0 0 0 0 0 0 0"   # sigma2p
+run_postproc="1 0 0 0 0 0 0 0 0 0"    # sigma2p
 #run_postproc="0 1 0 0 0 0 0 0 0 0"   # bias
 #run_postproc="0 0 0 0 0 0 0 0 0 0 1" # bias2
 #run_postproc="0 0 1 0 0 0 0 0 0 0"   # prpct
