f5a77053f44be505577651b5e02554efd3c53652
Leidinice Silva <leidinicesilva@gmail.com>
2024-05-17 16:57:35 +0200

    update scripts

---
+++ ictp/regcm_post_v1/sam_3km/plot/call_plot_sa-ws.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH -N 1 
+#SBATCH -t 24:00:00
+#SBATCH -A ICT23_ESP
+#SBATCH --qos=qos_prio
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=mda_silv@ictp.it
+#SBATCH -p skl_usr_prod
+
+#__author__      = 'Leidinice Silva'
+#__email__       = 'leidinicesilva@gmail.com'
+#__date__        = 'Mar 04, 2024'
+#__description__ = 'Call python code to plot'
+
+{
+
+echo
+echo "--------------- INIT PLOT ----------------"
+
+source /marconi/home/userexternal/ggiulian/STACK22/env2022
+set -eo pipefail
+
+DIR_IN="/marconi/home/userexternal/mdasilva/github_projects/pypostdoc/ws-sa"
+cd ${DIR_IN}
+
+python3 plot_dendrogram_stations_sam_v2.py
+
+echo
+echo "--------------- THE END PLOT ----------------"
+
+}
