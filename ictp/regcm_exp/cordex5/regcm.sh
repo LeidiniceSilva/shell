7f535f73dccae474a5b136e787c8464337fd78d8
Maria Leidinice da Silva <mdasilva@login01.leonardo.local>
2024-12-30 16:19:07 +0100

    update branch

---
+++ ictp/regcm_exp/cordex5/regcm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -o logs/rcm_SLURM.out
+#SBATCH -e logs/rcm_SLURM.err
+#SBATCH -N 30 ##--ntasks-per-node=20 #--mem=63G ##esp1
+#SBATCH --ntasks-per-node=112
+#SBATCH -t 24:00:00
+#SBATCH -J CSAM-3
+#SBATCH -A ICT23_ESP_1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=mda_silv@ictp.it
+#SBATCH -p dcgp_usr_prod
+
+{
+set -eo pipefail
+
+#module purge
+source /leonardo/home/userexternal/ggiulian/modules
+
+nl=$1
+mpirun ./bin/regcmMPICLM45 $nl
+}
+
