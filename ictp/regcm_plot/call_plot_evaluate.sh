d41c28c1a1d42480ab6576022dad37472cd4e621
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2025-05-15 17:15:23 +0200

    update scripts

--- ictp/regcm_plot/call_plot_evaluate.sh
+++ ictp/regcm_plot/call_plot_evaluate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#SBATCH -N 4 
-#SBATCH -t 24:00:00
-#SBATCH -A ICT23_ESP
-#SBATCH --qos=qos_prio
-#SBATCH --mail-type=FAIL
+#SBATCH -A ICT25_ESP
+#SBATCH -p dcgp_usr_prod
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=112
+#SBATCH -t 1-00:00:00
+#SBATCH -J Plot
+#SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=mda_silv@ictp.it
-#SBATCH -p skl_usr_prod
 
 #__author__      = 'Leidinice Silva'
 #__email__       = 'leidinicesilva@gmail.com'
@@ -15,13 +16,13 @@
 
 {
 
-source /marconi/home/userexternal/ggiulian/STACK22/env2022
+source /leonardo/home/userexternal/ggiulian/modules_gfortran
 set -eo pipefail
 
 echo
 echo "--------------- INIT PLOT ----------------"
 
-DIR_IN="/marconi/home/userexternal/mdasilva/github_projects/pypostdoc/sam_3km/sam_3km_evaluate"
+DIR_IN="/leonardo/home/userexternal/mdasilva/github_projects/pypostdoc/sam_3km/evaluate"
 cd ${DIR_IN}
 
 #python3 plot_graph_annual_cycle.py
--- ictp/regcm_post_v1/sam_3km/evaluate/rcm/postproc_cdo_regcm5_srf_freq_int_1hr.sh
+++ ictp/regcm_post_v1/sam_3km/evaluate/rcm/postproc_cdo_regcm5_srf_freq_int_1hr.sh
@@ -32,7 +32,7 @@ VAR="pr"
 TH=0.5
 
 DIR_IN="/leonardo/home/userexternal/mdasilva/leonardo_work/SAM-3km/output"
-DIR_OUT="/leonardo/home/userexternal/mdasilva/leonardo_work/SAM-3km/postproc/evaluate"
+DIR_OUT="/leonardo/home/userexternal/mdasilva/leonardo_work/SAM-3km/postproc/evaluate/rcm"
 BIN="/leonardo/home/userexternal/mdasilva/RegCM/bin"
 
 echo
