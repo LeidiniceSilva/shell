9d1ae139f5f27ddc2dd1334e347a429eca418fc3
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2025-06-25 19:02:10 +0200

    update scripts

--- ictp/regcm_post_v1/cordex5/evaluate/rcm/postproc_cdo_regcm5_srf_p1.sh
+++ ictp/regcm_post_v1/cordex5/evaluate/rcm/postproc_cdo_regcm5_srf_p1.sh
@@ -31,7 +31,8 @@ IYR=$( echo $YR | cut -d- -f1 )
 FYR=$( echo $YR | cut -d- -f2 )
 SEASON_LIST="DJF MAM JJA SON"
 
-VAR_LIST="pr tas tasmax tasmin cll clm clh clt evspsblpot rlds cape cin"
+VAR_LIST="cape cin"
+#VAR_LIST="pr tas tasmax tasmin cll clm clh clt evspsblpot rlds cape cin"
 
 DIR_OUT="/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5/postproc/evaluate/rcm"
 BIN="/leonardo/home/userexternal/mdasilva/RegCM/bin"
@@ -83,8 +84,7 @@ for VAR in ${VAR_LIST[@]}; do
     
     echo 
     echo "Delete files"
-    rm ${VAR}_${DOMAIN}_RegCM5_day_${YR}.nc
-    rm ${VAR}_${DOMAIN}_RegCM5_mon_${YR}.nc
+    rm ${VAR}_${DOMAIN}_RegCM5_*_${YR}.nc
     rm ${VAR}_${DOMAIN}_RegCM5_mon_${YR}_lonlat.nc
       
 done
