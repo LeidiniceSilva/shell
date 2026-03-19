0dafb48a3dd6edd9874866324f48c965682a7031
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2026-03-19 15:04:41 +0100

    update scripts

--- ictp/regcm_post_v1/moaap/postproc_mergefile_p1.sh
+++ ictp/regcm_post_v1/moaap/postproc_mergefile_p1.sh
@@ -21,34 +21,34 @@ CDO(){
   cdo -O -L -f nc4 -z zip $@
 }
 
-DOMAIN="CSAM-3"
+DOMAIN="EURR-3"
 EXP="ERA5_evaluation_r1i1p1f1_ICTP_RegCM5-0_v1-r1"
 
 if [ ${DOMAIN} = 'CAR-3'  ]; then
-	DIR_I="/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5/ERA5/ERA5-CSAM-3/CORDEX-CMIP6/DD/CSAM-3/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr"
-	
+	DIR_I="/leonardo_work/ICT26_ESP/CORDEX-CMIP6/DD/CAR-4/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr"
 elif [ ${DOMAIN} = 'CSAM-3'  ]; then
 	DIR_I="/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5/ERA5/ERA5-CSAM-3/CORDEX-CMIP6/DD/CSAM-3/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr"
+elif [ ${DOMAIN} = 'EURR-3'  ]; then
+	DIR_I="/leonardo_work/ICT26_ESP/jdeleeuw/EURR-3/ERA5/high_soil_moisture/ERA5/EURR-3/postproc/CORDEX-CMIP6/DD/EURR-3/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr"
 else
-	DIR_I="/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5/ERA5/ERA5-CSAM-3/CORDEX-CMIP6/DD/CSAM-3/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr"
+	echo "Error: Unknown DOMAIN = ${DOMAIN}"
+	exit 1
 fi	
 
-DIR_II="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/${DOMAIN}/input/Tb"
-
-DIR_OUT="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/${DOMAIN}/input"
+DIR_II="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/${DOMAIN}/input"
 
 echo
-cd ${DIR_OUT}
-echo ${DIR_OUT}
+cd ${DIR_II}
+echo ${DIR_II}
 
 echo
 echo "--------------- INIT POSPROCESSING MODEL ----------------"
 
 CDO mergetime ${DIR_I}/pr_* pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc
-CDO mergetime ${DIR_II}/Tb_* Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
-CDO merge pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+CDO mergetime /Tb/Tb_* Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
 
 echo
 echo "--------------- THE END POSPROCESSING MODEL ----------------"
 
 }
+
