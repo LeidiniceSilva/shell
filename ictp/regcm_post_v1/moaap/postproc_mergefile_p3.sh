7b16e0d48584711c50ad3920b37dc4be044681d6
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2026-03-24 14:39:13 +0100

    update branch

---
+++ ictp/regcm_post_v1/moaap/postproc_mergefile_p3.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#SBATCH -A ICT26_ESP
+#SBATCH -p dcgp_usr_prod
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=112
+#SBATCH -t 1-00:00:00
+#SBATCH -J Postproc
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=mda_silv@ictp.it
+
+#__author__      = 'Leidinice Silva'
+#__email__       = 'leidinicesilva@gmail.com'
+#__date__        = 'Nov 20, 2023'
+#__description__ = 'Posprocessing the RegCM5 output with CDO'
+
+{
+set -eo pipefail
+
+CDO(){
+  cdo -O -L -f nc4 -z zip $@
+}
+
+DOMAIN="CAR-4"
+EXP="ERA5_evaluation_r1i1p1f1_ICTP_RegCM5-0_v1-r1"
+
+if [ ${DOMAIN} = 'CAR-3'  ]; then
+	DIR_I="/leonardo_work/ICT26_ESP/CORDEX-CMIP6/DD/CAR-4/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr/v20260308"
+elif [ ${DOMAIN} = 'CSAM-3'  ]; then
+	DIR_I="/leonardo_work/ICT26_ESP/CORDEX-CMIP6/DD/CSAM-3/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr"
+elif [ ${DOMAIN} = 'EURR-3'  ]; then
+	DIR_I="/leonardo_work/ICT26_ESP/CORDEX-CMIP6/DD/EURR-3/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr/v20251218"
+else
+	echo "Error: Unknown DOMAIN = ${DOMAIN}"
+	exit 1
+fi	
+
+DIR_II="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/${DOMAIN}/input"
+
+echo
+cd ${DIR_II}
+echo ${DIR_II}
+
+echo
+echo "--------------- INIT POSPROCESSING MODEL ----------------"
+
+CDO mergetime ${DIR_I}/pr_* pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+CDO mergetime ${DIR_II}/Tb/Tb_* Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+
+FILE_IN_I=pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+FILE_IN_II=Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+
+YEARS=$(seq 2000 2009)
+for YEAR in $YEARS; do
+    for MON in $(seq -w 1 12); do
+
+	echo "Processing ${YEAR} ${MON}"
+
+        FILE_OUT_I=pr_${DOMAIN}_${EXP}_1hr_${YEAR}${MON}0100.nc
+        FILE_OUT_II=Tb_${DOMAIN}_${EXP}_1hr_${YEAR}${MON}0100.nc
+        FILE_OUT_III=${DOMAIN}_${EXP}_1hr_${YEAR}${MON}0100.nc
+
+        cdo seldate,${YEAR}-${MON}-01,${YEAR}-${MON}-31 $FILE_IN_I $FILE_OUT_I
+        cdo seldate,${YEAR}-${MON}-01,${YEAR}-${MON}-31 $FILE_IN_II $FILE_OUT_II
+        cdo merge $FILE_OUT_I $FILE_OUT_II $FILE_OUT_III
+
+	echo "Processed $FILE_OUT_III"
+
+    done
+done
+
+echo
+echo "--------------- THE END POSPROCESSING MODEL ----------------"
+
+}
+
