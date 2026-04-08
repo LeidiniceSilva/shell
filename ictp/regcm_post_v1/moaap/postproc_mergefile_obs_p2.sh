560fdcb1f7003b142880347595d99e635cfc5848
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2026-04-08 16:01:32 +0200

    update scripts

---
+++ ictp/regcm_post_v1/moaap/postproc_mergefile_obs_p2.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#SBATCH -A ICT26_ESP
+#SBATCH -p dcgp_usr_prod
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=112
+#SBATCH -t 1-00:00:00
+#SBATCH -J Postproc
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=mda_silv@ictp.it
+
+#__author__      = 'Leidinice Silva'
+#__email__       = 'leidinicesilva@gmail.com'
+#__date__        = 'Nov 20, 2023'
+#__description__ = 'Posprocessing the RegCM5 output with CDO'
+
+{
+set -eo pipefail
+
+CDO(){
+  cdo -O -L -f nc4 -z zip $@
+}
+
+DOMAIN="EURR-3"
+EXP="ERA5_reanalysis"
+DIR_I="/leonardo/home/userexternal/mdasilva/leonardo_work/OBS/ERA5/1hr"
+DIR_II="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/${DOMAIN}/input"
+
+echo
+cd ${DIR_II}
+echo ${DIR_II}
+
+echo
+echo "--------------- INIT POSPROCESSING ----------------"
+
+CDO mergetime ${DIR_I}/Tb_* ${DIR_I}/Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+
+FILE_IN_I=${DIR_I}/pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+FILE_IN_II=${DIR_I}/Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+
+YEARS=$(seq 2000 2009)
+for YEAR in $YEARS; do
+    for MON in $(seq -w 1 12); do
+
+	echo "Processing ${YEAR} ${MON}"
+
+        FILE_OUT_I=pr_${DOMAIN}_${EXP}_1hr_${YEAR}${MON}0100.nc
+        FILE_OUT_II=Tb_${DOMAIN}_${EXP}_1hr_${YEAR}${MON}0100.nc
+        FILE_OUT_III=${DOMAIN}_${EXP}_1hr_${YEAR}${MON}0100.nc
+
+        cdo seldate,${YEAR}-${MON}-01,${YEAR}-${MON}-31 $FILE_IN_I $FILE_OUT_I
+        cdo seldate,${YEAR}-${MON}-01,${YEAR}-${MON}-31 $FILE_IN_II $FILE_OUT_II
+        cdo merge $FILE_OUT_I $FILE_OUT_II $FILE_OUT_III
+
+	echo "Processed $DIR_OUT_III"
+
+    done
+done
+
+echo
+echo "--------------- THE END POSPROCESSING MODEL ----------------"
+
+}
+
+echo
+echo "--------------- THE END POSPROCESSING MODEL ----------------"
+
+}
+
