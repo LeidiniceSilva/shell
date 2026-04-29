4b674111f47ec812beaa571f4e2a040ba3174bac
Maria Leidinice da Silva <leidinicesilva@gmail.com>
2026-04-29 18:38:17 +0200

    update scripts

--- ictp/regcm_post_v1/moaap/postproc_mergefile_rcm_p1.sh
+++ ictp/regcm_post_v1/moaap/postproc_mergefile_rcm_p1.sh
@@ -21,11 +21,11 @@ CDO(){
   cdo -O -L -f nc4 -z zip $@
 }
 
-DOMAIN="CAR-4"  # CSAM-3 EURR-3
+DOMAIN="CAR-4"  # CAR-4 CSAM-3 EURR-3
 EXP="ERA5_evaluation_r1i1p1f1_ICTP_RegCM5-0_v1-r1"
 
-if [ ${DOMAIN} = 'CAR-3'  ]; then
-	DIR_I="/leonardo_work/ICT26_ESP/CORDEX-CMIP6/DD/CAR-4/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr"
+if [ ${DOMAIN} = 'CAR-4'  ]; then
+	DIR_I="/leonardo_work/ICT26_ESP/CORDEX-CMIP6/DD/CAR-4/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr/v20260308"
 elif [ ${DOMAIN} = 'CSAM-3'  ]; then
 	DIR_I="/leonardo/home/userexternal/mdasilva/leonardo_work/CORDEX5/ERA5/ERA5-CSAM-3/CORDEX-CMIP6/DD/CSAM-3/ICTP/ERA5/evaluation/r1i1p1f1/RegCM5-0/v1-r1/1hr/pr"
 elif [ ${DOMAIN} = 'EURR-3'  ]; then
@@ -35,7 +35,7 @@ else
 	exit 1
 fi	
 
-DIR_II="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/${DOMAIN}/input"
+DIR_II="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/CPMs/${DOMAIN}/input"
 
 echo
 cd ${DIR_II}
@@ -44,8 +44,10 @@ echo ${DIR_II}
 echo
 echo "--------------- INIT POSPROCESSING MODEL ----------------"
 
-CDO mergetime ${DIR_I}/pr_* pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc
-CDO mergetime /leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/EURR-3/input/Tb/Tb_* Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+CDO mergetime ${DIR_I}/pr_* pr_${DOMAIN}_${EXP}_mmhr_2000-2009.nc
+CDO mulc,3600 pr_${DOMAIN}_${EXP}_mmhr_2000-2009.nc pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc
+
+CDO mergetime ${DIR_II}/Tb/Tb_* Tb_${DOMAIN}_${EXP}_1hr_1999-2009.nc
 
 echo
 echo "--------------- THE END POSPROCESSING MODEL ----------------"
--- ictp/regcm_post_v1/moaap/postproc_mergefile_rcm_p2.sh
+++ ictp/regcm_post_v1/moaap/postproc_mergefile_rcm_p2.sh
@@ -21,9 +21,9 @@ CDO(){
   cdo -O -L -f nc4 -z zip $@
 }
 
-DOMAIN="CAR-4" # CSAM-3 EURR-3
+DOMAIN="CSAM-3" # CSAM-3 EURR-3
 EXP="ERA5_evaluation_r1i1p1f1_ICTP_RegCM5-0_v1-r1"
-DIR_IN="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/${DOMAIN}/input"
+DIR_IN="/leonardo/home/userexternal/mdasilva/leonardo_work/MOAAP/CPMs/${DOMAIN}"
 
 echo
 cd ${DIR_IN}
@@ -32,10 +32,7 @@ echo ${DIR_IN}
 echo
 echo "--------------- INIT POSPROCESSING MODEL ----------------"
 
-FILE_IN_I=pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc
-FILE_IN_II=Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc
-
-YEARS=$(seq 2000 2009)
+YEARS=$(seq 2001 2009)
 for YEAR in $YEARS; do
     for MON in $(seq -w 1 12); do
 
@@ -45,9 +42,9 @@ for YEAR in $YEARS; do
         FILE_OUT_II=Tb_${DOMAIN}_${EXP}_1hr_${YEAR}${MON}0100.nc
         FILE_OUT_III=${DOMAIN}_${EXP}_1hr_${YEAR}${MON}0100.nc
 
-        cdo seldate,${YEAR}-${MON}-01,${YEAR}-${MON}-31 $FILE_IN_I $FILE_OUT_I
-        cdo seldate,${YEAR}-${MON}-01,${YEAR}-${MON}-31 $FILE_IN_II $FILE_OUT_II
-        cdo merge $FILE_OUT_I $FILE_OUT_II $FILE_OUT_III
+        CDO seldate,${YEAR}-${MON}-01,${YEAR}-${MON}-31 pr_${DOMAIN}_${EXP}_1hr_2000-2009.nc $FILE_OUT_I
+        CDO seldate,${YEAR}-${MON}-01,${YEAR}-${MON}-31 Tb_${DOMAIN}_${EXP}_1hr_2000-2009.nc $FILE_OUT_II
+        CDO merge $FILE_OUT_I $FILE_OUT_II ${DIR_IN}/input/$FILE_OUT_III
 
 	echo "Processed $DIR_OUT_III"
 
